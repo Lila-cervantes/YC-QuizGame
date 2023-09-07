0857440b9e56f9ff5b6586d6bd9671a704f55b94
Lila-cervantes <139878924+Lila-cervantes@users.noreply.github.com>
2023-09-07 12:58:17 -0500

    Delete scores.js

--- scores.js
+++
@@ -1,9 +0,0 @@
-var reset = document.querySelecotr('#clear');
-
-//EventListener will clear scores and store them.
-reset.addEventListener('click', function () {
-    location.reload();
-    localStorage.clear();
-})
-
-
